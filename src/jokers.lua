599f32c1caeee6f17e64d849c36768435f77d418
Sebastián Venturino <venturinosebastian@gmail.com>
2025-05-25 23:42:34 -0300

    blue/brain area check

--- src/jokers.lua
+++ src/jokers.lua
@@ -3876,27 +3876,29 @@ SMODS.Joker {
     cost = 10,
     pos = { x = 0, y = 3 },
     loc_vars = function(self, info_queue, card)
-        local other_joker
-        for i = 1, #G.jokers.cards do
-            if G.jokers.cards[i] == card then other_joker = G.jokers.cards[i + 1] end
-        end
-        local compatible = other_joker and other_joker ~= card and other_joker.config.center.blueprint_compat
-        main_end = (card.area and card.area == G.jokers) and {
-            {
-                n = G.UIT.C,
-                config = { align = "bm", minh = 0.4 },
-                nodes = {
-                    {
-                        n = G.UIT.C,
-                        config = { ref_table = card, align = "m", colour = compatible and mix_colours(G.C.GREEN, G.C.JOKER_GREY, 0.8) or mix_colours(G.C.RED, G.C.JOKER_GREY, 0.8), r = 0.05, padding = 0.06 },
-                        nodes = {
-                            { n = G.UIT.T, config = { text = ' ' .. localize('k_' .. (compatible and 'compatible' or 'incompatible')) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
+        if card.area and card.area == G.jokers then
+            local other_joker
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] == card then other_joker = G.jokers.cards[i + 1] end
+            end
+            local compatible = other_joker and other_joker ~= card and other_joker.config.center.blueprint_compat
+            main_end = {
+                {
+                    n = G.UIT.C,
+                    config = { align = "bm", minh = 0.4 },
+                    nodes = {
+                        {
+                            n = G.UIT.C,
+                            config = { ref_table = card, align = "m", colour = compatible and mix_colours(G.C.GREEN, G.C.JOKER_GREY, 0.8) or mix_colours(G.C.RED, G.C.JOKER_GREY, 0.8), r = 0.05, padding = 0.06 },
+                            nodes = {
+                                { n = G.UIT.T, config = { text = ' ' .. localize('k_' .. (compatible and 'compatible' or 'incompatible')) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
+                            }
                         }
                     }
                 }
             }
-        } or nil
-        return { main_end = main_end }
+            return { main_end = main_end }
+        end
     end,
     calculate = function(self, card, context)
         local other_joker = nil
@@ -4426,24 +4428,26 @@ SMODS.Joker {
     cost = 10,
     pos = { x = 7, y = 7 },
     loc_vars = function(self, info_queue, card)
-        local compatible = G.jokers.cards[1] and G.jokers.cards[1] ~= card and
-            G.jokers.cards[1].config.center.blueprint_compat
-        main_end = (card.area and card.area == G.jokers) and {
-            {
-                n = G.UIT.C,
-                config = { align = "bm", minh = 0.4 },
-                nodes = {
-                    {
-                        n = G.UIT.C,
-                        config = { ref_table = card, align = "m", colour = compatible and mix_colours(G.C.GREEN, G.C.JOKER_GREY, 0.8) or mix_colours(G.C.RED, G.C.JOKER_GREY, 0.8), r = 0.05, padding = 0.06 },
-                        nodes = {
-                            { n = G.UIT.T, config = { text = ' ' .. localize('k_' .. (compatible and 'compatible' or 'incompatible')) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
+        if card.area and card.area == G.jokers then
+            local compatible = G.jokers.cards[1] and G.jokers.cards[1] ~= card and
+                G.jokers.cards[1].config.center.blueprint_compat
+            main_end = {
+                {
+                    n = G.UIT.C,
+                    config = { align = "bm", minh = 0.4 },
+                    nodes = {
+                        {
+                            n = G.UIT.C,
+                            config = { ref_table = card, align = "m", colour = compatible and mix_colours(G.C.GREEN, G.C.JOKER_GREY, 0.8) or mix_colours(G.C.RED, G.C.JOKER_GREY, 0.8), r = 0.05, padding = 0.06 },
+                            nodes = {
+                                { n = G.UIT.T, config = { text = ' ' .. localize('k_' .. (compatible and 'compatible' or 'incompatible')) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
+                            }
                         }
                     }
                 }
             }
-        } or nil
-        return { main_end = main_end }
+            return { main_end = main_end }
+        end
     end,
     calculate = function(self, card, context)
         local ret = SMODS.blueprint_effect(card, G.jokers.cards[1], context)
