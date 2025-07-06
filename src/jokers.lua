4429d66efc0c074a3acc9d3147019a4e0fe8bc0c
Sebastián Venturino <venturinosebastian@gmail.com>
2025-07-06 20:44:04 -0300

    SMODS.is_eternal

--- src/jokers.lua
+++ src/jokers.lua
@@ -430,7 +430,7 @@ SMODS.Joker {
                     break
                 end
             end
-            if my_pos and G.jokers.cards[my_pos + 1] and not G.jokers.cards[my_pos + 1].ability.eternal and not G.jokers.cards[my_pos + 1].getting_sliced then
+            if my_pos and G.jokers.cards[my_pos + 1] and not SMODS.is_eternal(G.jokers.cards[my_pos + 1], card) and not G.jokers.cards[my_pos + 1].getting_sliced then
                 local sliced_card = G.jokers.cards[my_pos + 1]
                 sliced_card.getting_sliced = true -- Make sure to do this on destruction effects
                 G.GAME.joker_buffer = G.GAME.joker_buffer - 1
@@ -1768,7 +1768,7 @@ SMODS.Joker {
             card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmult_gain
             local destructable_jokers = {}
             for i = 1, #G.jokers.cards do
-                if G.jokers.cards[i] ~= card and not G.jokers.cards[i].ability.eternal and not G.jokers.cards[i].getting_sliced then
+                if G.jokers.cards[i] ~= card and not SMODS.is_eternal(G.jokers.cards[i], card) and not G.jokers.cards[i].getting_sliced then
                     destructable_jokers[#destructable_jokers + 1] =
                         G.jokers.cards[i]
                 end
